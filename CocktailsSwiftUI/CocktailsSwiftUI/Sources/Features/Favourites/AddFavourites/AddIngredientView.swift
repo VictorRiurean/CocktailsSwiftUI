ff132133063106d503ee755c1c30b75f41a1c7e7
Victor Riurean <v.riurean@innovattic.com>
2023-02-12 17:15:54 +0100

    Update addIngredientView proportions dynamically

--- CocktailsSwiftUI/CocktailsSwiftUI/Sources/Features/Favourites/AddFavourites/AddIngredientView.swift
+++ CocktailsSwiftUI/CocktailsSwiftUI/Sources/Features/Favourites/AddFavourites/AddIngredientView.swift
@@ -18,12 +18,26 @@ struct AddIngredientView: View {
     // MARK: Body
     
     var body: some View {
-        HStack {
-            TextField("Measure", text: $measure)
-            
-            Divider()
-            
-            TextField("Ingredient", text: $name)
+        /// The geometryProxy's size attribute returns the size of it's
+        /// container view, thus allowing us to give dynamically set
+        /// values to the elements inside the HStack
+        GeometryReader { geo in
+            HStack {
+                TextField("Measure", text: $measure)
+                    .frame(width: geo.size.width * 0.3)
+                
+                Divider()
+                
+                TextField("Ingredient", text: $name)
+                    .frame(width: geo.size.width * 0.6)
+                    .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 0))
+            }
         }
+    }
+}
+
+struct AddIngredientView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddIngredientView(measure: .constant(""), name: .constant(""))
     }
 }

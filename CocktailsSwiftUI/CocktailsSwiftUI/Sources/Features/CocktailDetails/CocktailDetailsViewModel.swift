1dbf4fe546ef647b59dd82c9306829ea100f3d15
Victor Riurean <v.riurean@innovattic.com>
2023-10-11 14:20:34 +0200

    Switch to Observation

--- CocktailsSwiftUI/CocktailsSwiftUI/Sources/Features/CocktailDetails/CocktailDetailsViewModel.swift
+++ CocktailsSwiftUI/CocktailsSwiftUI/Sources/Features/CocktailDetails/CocktailDetailsViewModel.swift
@@ -13,16 +13,11 @@ struct CocktailDetailsViewModel {
     // MARK: Private properties
     
     private let service = WebService.shared
-    private let databaseManager = DatabaseManager.shared
     
     
     // MARK: Public methods
     
     func fetchDrink(name: String) async -> CocktailResponse {
         await service.fetchDrink(with: name)
-    }
-    
-    func addDrinksToCoreData(drinks: [CocktailResponse], context: NSManagedObjectContext) {
-        databaseManager.addDrinksToCoreData(drinks: drinks, context: context)
     }
 }
--- CocktailsSwiftUI/CocktailsSwiftUI/Sources/Features/Cocktails/CocktailsViewModel.swift
+++ CocktailsSwiftUI/CocktailsSwiftUI/Sources/Features/Cocktails/CocktailsViewModel.swift
@@ -6,16 +6,17 @@
 //
 
 import CoreData
+import Observation
 import SwiftUI
-/// Marked as MainActor because changes to it need to be published on main thread. See:
-/// https://www.hackingwithswift.com/quick-start/concurrency/how-to-use-mainactor-to-run-code-on-the-main-queue
-@MainActor
-class CocktailsViewModel: ObservableObject {
+
+
+@Observable
+class CocktailsViewModel {
     
     // MARK: State
     
-    @Published var isLoading = false
-    @Published var loadedLetters: [String] = []
+    var isLoading = false
+    var loadedLetters: [String] = []
     
     
     // MARK: Public properties
@@ -27,7 +28,6 @@ class CocktailsViewModel: ObservableObject {
     // MARK: Private properties
     
     private let service = WebService.shared
-    private let databaseManager = DatabaseManager.shared
     
     private var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
@@ -71,10 +71,5 @@ class CocktailsViewModel: ObservableObject {
         isLoading = false
         
         return drinks
-    }
-    /// We have to get the context passed from the parent view. If we were to declare
-    /// an environment variable here it wouldn't work (we get purple warnings).
-    func addDrinksToCoreData(drinks: [CocktailResponse], context: NSManagedObjectContext) {
-        databaseManager.addDrinksToCoreData(drinks: drinks, context: context)
     }
 }
